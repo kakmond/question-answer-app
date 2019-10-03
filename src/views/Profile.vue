1db1488bcb16335b85f473c498f49851331552ed
kakmond <wongsathorn.pa@ku.th>
2019-10-03 18:43:44 +0200

    add simple profile page

---
+++ src/views/Profile.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="text-center">
+    <h1 class="text-danger font-weight-bold display-4">Mond</h1>
+    <h4 class="font-weight-light text-muted">@mond</h4>
+    <br />
+    <ul class="nav-fill nav nav-tabs" role="tablist">
+      <li class="nav-item">
+        <a
+          class="nav-link active"
+          id="home-tab"
+          data-toggle="tab"
+          href="#home"
+          role="tab"
+          aria-controls="home"
+          aria-selected="true"
+        >Questions</a>
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          id="profile-tab"
+          data-toggle="tab"
+          href="#profile"
+          role="tab"
+          aria-controls="profile"
+          aria-selected="false"
+        >Answers</a>
+      </li>
+    </ul>
+    <div class="tab-content">
+      <div
+        class="tab-pane fade show active"
+        id="home"
+        role="tabpanel"
+        aria-labelledby="home-tab"
+      >...</div>
+      <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+    </div>
+  </div>
+</template>
