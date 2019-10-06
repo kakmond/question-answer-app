b0631e5bd873f506981eb53d74eaeb08311f90d0
kakmond <wongsathorn.pa@ku.th>
2019-10-06 23:03:17 +0200

    remove home page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import About from './views/About.vue'
 import Questions from './views/Questions.vue'
 import Question from './views/Question.vue'
@@ -16,11 +15,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
     {
       path: '/about',
       name: 'about',
